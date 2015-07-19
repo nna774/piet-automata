8568047acb00b81feec67c67ecd1a9dfdf0a8175
NoNameA 774 <nonamea.774@gmail.com>
2015-07-20 04:09:47 +0900

    add space

--- app.js
+++ app.js
@@ -217,15 +217,15 @@ function genCodeMap(code) {
             newCode[0].push({ op: OP.push2 });
             while (true) {
               if (d * d + sum < tar) {
-                newCode[0].push({ op: OP.dup});
-                newCode[0].push({ op: OP.mul});
+                newCode[0].push({ op: OP.dup });
+                newCode[0].push({ op: OP.mul });
                 d *= d;
               } else if (d * 2 + sum < tar) {
-                newCode[0].push({ op: OP.dup});
-                newCode[0].push({ op: OP.add});
+                newCode[0].push({ op: OP.dup });
+                newCode[0].push({ op: OP.add });
                 d *= 2;
               } else {
-                newCode[0].push({ op: OP.add});
+                newCode[0].push({ op: OP.add });
                 sum += d;
                 break;
               }
